a370172a349ee39b471333eaa2fa6cafd7eb0912
Quyum Kehinde <Quyumkehinde@gmail.com>
2021-04-02 15:15:29 -0700

    Addition of hyperlink to apply for jobs

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 fetch("https://remotive.io/api/remote-jobs?search=seo&category=marketing")
         .then((res) => res.json())
         .then(function(data){
-            console.log(data)
             jobs = data.jobs;
             jobs.map(job => {
                 var company = job.company_name;
                 var locate = job.candidate_required_location;
                 var title = job.title;
+                var url = job.url;
                 var jobNode = `<div class="jobinfo">
                     <p class="company">${company}</p>
                     <p class="location">${locate}</p>
-                    <p class="position">${title}</p>
+                    <p class="position"><a href="${url}">${title}</a></p>
 
                 </div>`;
                 document.querySelector(".jobs").innerHTML = document.querySelector(".jobs").innerHTML + jobNode;
