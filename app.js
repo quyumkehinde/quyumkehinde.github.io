8b006887864b43afd8048370b0ed0487b3184ad6
Quyum Kehinde <Quyumkehinde@gmail.com>
2021-04-06 14:57:10 -0700

    Made some changes to the stylesheet

--- app.js
+++ app.js
@@ -4,10 +4,41 @@ fetch("https://remotive.io/api/remote-jobs?search=seo&category=marketing")
         jobs = data.jobs;
         let latestDate = data.jobs[0].publication_date;
         latestDate = latestDate.substr(0, 10);
+        let monthCode = latestDate.substr(5, 2);
+        let month;
+        if(monthCode == "01"){
+            month = "January;"
+        }else if(monthCode == "02"){
+            month = "February";
+        }else if(monthCode == "03"){
+            month = "March";
+        }else if(monthCode == "04"){
+            month = "April";
+        }else if(monthCode == "05"){
+            month = "May";
+        }else if(monthCode == "06"){
+            month = "June";
+        }else if(monthCode == "07"){
+            month = "July";
+        }else if(monthCode == "08"){
+            month = "August";
+        }else if(monthCode == "08"){
+            month = "September"
+        }else if(monthCode == "10"){
+            month = "October";
+        }else if(monthCode == "11"){
+            month = "November";
+        }else if(monthCode == "12"){
+            month = "December";
+        }else{
+
+        }
+        let day = latestDate.substr(8, 2);
+        let year = latestDate.substr(0, 4);
+        latestDate = month + " " + day + ", " + year;
         const dateNode = document.getElementsByClassName("date")[0];
         dateNode.innerHTML = dateNode.innerHTML + latestDate;
         jobs.map(job => {
-            console.log(job)
             var company = job.company_name;
             var locate = job.candidate_required_location;
             var date = job.publication_date;
